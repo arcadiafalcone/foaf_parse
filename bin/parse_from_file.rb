0ec7d7c8198a63449e81780aa7c9e54ca25e021b
Arcadia Falcone <arcadia@stanford.edu>
2016-06-28 15:57:25 -0700

    Experiment with parse script

--- bin/parse_from_file.rb
+++ bin/parse_from_file.rb
@@ -1,8 +1,26 @@
 require 'rdf'
-require 'rdf/raptor'
+require 'linkeddata'
 require 'sparql'
-require 'net/http'
-require 'openssl'
+require 'set'
 
 graph = RDF::Graph.load("foaf_files/arcadia_foaf.rdf")
-puts graph
+puts graph.inspect
+
+query = "PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+  SELECT *
+  WHERE {?s foaf:knows ?o}"
+
+result_set = Set.new
+
+puts 'iteration'
+sse = SPARQL.parse(query)
+sse.execute(graph) do |result|
+  puts result.o
+  result_set.add(result.o)
+  rdf = RDF::Resource(RDF::URI.new(result.o))
+  graph.load(rdf)
+end
+puts 'set'
+result_set.each do |r|
+  puts r
+end
